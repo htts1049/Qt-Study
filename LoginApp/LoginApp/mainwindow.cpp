1cef662e00102be5e18100b495dfdbf99813a76c
htts1049 <htts1049@gmail.com>
2024-11-24 23:43:07 +0900

    1. Date : 2024-11-24 2. TID : - QPixmap 을 이용해, QLabel 에 Image 삽입 - setPix 메소드를 이용해, Image 의 크기를 조정 - width, height 메소드를 이용해, Image 의 크기를 QLabel 크기만큼 조정

--- LoginApp/LoginApp/mainwindow.cpp
+++ LoginApp/LoginApp/mainwindow.cpp
@@ -1,12 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "QMessageBox"
+#include <QMessageBox>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("C:/Users/Administrator/pic1.jpg");
+    int w = ui->label_pic->width();
+    int h = ui->label_pic->height();
+    ui->label_pic->setPixmap(pix.scaled(w, h, Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
